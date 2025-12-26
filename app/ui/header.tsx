ce86fd577612f05e871f46d9485fd62d8cbd0801
guniism <gun16110@hotmail.com>
2025-12-26 09:56:22 +0700

    scroll smooth

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,27 +1,31 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Header(){
-    return (
-        <div className="absolute h-14 max-w-3xl w-full m-5 flex justify-center px-5">
-            <div className="bg-con max-w-2xl w-full rounded-lg border border-bd flex flex-row justify-between items-center px-4">
-                {/* <h1 className="text-4xl">Guniism</h1> */}
-                <Link href="#">
-                    <Image 
-                        src="/Logo.png" 
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="w-full h-auto"
-                    />
-                </Link>
-                <ul className="flex flex-row space-x-8 text-xl">
-                    {/* <li>Home</li> */}
-                    <li><Link href="#about">About</Link></li>
-                    <li><Link href="#project">Projects</Link></li>
-                    {/* <li>Uses</li> */}
-                </ul>
-            </div>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="absolute h-14 max-w-3xl w-full m-5 flex justify-center px-5">
+      <div className="bg-con max-w-2xl w-full rounded-lg border border-bd flex flex-row justify-between items-center px-4">
+        {/* <h1 className="text-4xl">Guniism</h1> */}
+        <Link href="#">
+          <Image
+            src="/Logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-full h-auto"
+          />
+        </Link>
+        <ul className="flex flex-row space-x-8 text-xl">
+          {/* <li>Home</li> */}
+          <li>
+            <Link href="#about">About</Link>
+          </li>
+          <li>
+            <Link href="#project">Projects</Link>
+          </li>
+          {/* <li>Uses</li> */}
+        </ul>
+      </div>
+    </div>
+  );
 }
